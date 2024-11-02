e4101f0b17ede699d9b234cd0e37efb25b1239f5
GH-X-ST <lihanchen12476@163.com>
2024-11-02 21:12:16 +0000

    Rewrite the entire Initial Sizing to find better result
    Still need iterations to define cruise altitude and best AR

--- conceptual design review/sections/A - initial sizing and baseline/ISAtemp.m
+++
@@ -1,7 +0,0 @@
-function T = ISAtemp(h)
-    if h < 11e3
-        T = 288.15 + -0.0065*h;  % Temperature decreases with altitude below 11 km
-    else
-        T = 216.65;  % Temperature is constant at 216.65 K above 11 km
-    end
-end
--- conceptual design review/sections/A - initial sizing and baseline/T_T0.m
+++ conceptual design review/sections/A - initial sizing and baseline/T_T0.m
@@ -1,10 +1,22 @@
 function T_T0 = T_T0(h)
-    rho = @(h) 1.225*ISApressure(h)/ISApressure(0)*ISAtemp(0)/ISAtemp(h);
-    rho0 = rho(0);
-    rho1 = rho(11e3);
-    if h < 11e3
-        T_T0 = (rho(h)/rho0)^0.7;
-    else
-        T_T0 = (rho(11e3)/rho0)^(-0.3)*rho(h)/rho(0);
-    end
+% This function is used to calculate thrust of a jet engine
+% at different altitude by ignoring all the effects of 
+% airspeed, which is applicable above around 30000ft with
+% Mach number larger than 0.6
+%
+% T_T0 = T_T0(h)
+%
+% outputs: T_T0 - Thrust ratio
+% inputs:  h    - altitude (m)
+
+[T0, c0, P0, rho0] = atmosisa(0);
+[T1, c1, P1, rho1] = atmosisa(11e3);
+[Th, ch, Ph, rhoh] = atmosisa(h);
+
+if h < 11e3
+    T_T0 = (rhoh / rho0) ^ 0.7;
+else
+    T_T0 = (rho1 / rho0) ^ (-0.3) * (rhoh / rho0);
+end
+
 end
